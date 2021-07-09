bf21da7e58b547c428badc61567f1977233ec89b
JBR official <85146090+jbrofficial@users.noreply.github.com>
2021-07-09 11:36:29 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -170,7 +170,7 @@ async def button(bot, cmd: CallbackQuery):
                         InlineKeyboardButton("🖋 Current Caption", callback_data = "status_data")
                        ], 
                        [
-                        InlineKeyboardButton("🍃 Follow Me", url="https://Instagram.com/proavipatil"),
+                        InlineKeyboardButton("🍃 Follow Me", url="https://Instagram.com/jbr__creation"),
                         InlineKeyboardButton("📕 About Me", callback_data="about_data")
                        ],
                        [
